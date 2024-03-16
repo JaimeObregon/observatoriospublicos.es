34cbad6acc9729ef941d81facbcc8cff85749081
Jaime Gómez-Obregón <417226+JaimeObregon@users.noreply.github.com>
2024-03-16 13:46:36 +0100

    Ponga aquí su publicidad

--- httpdocs/app.js
+++ httpdocs/app.js
@@ -1,18 +1,40 @@
 const types = [
   {
-    public: 'Público',
+    key: 'public',
+    name: 'Público',
   },
   {
-    mixed: 'Público-privado',
+    key: 'mixed',
+    name: 'Público-privado',
   },
 ]
 
 const observatories = [
   {
-    name: 'x',
-    website: '',
-    financiación: types.public,
+    name: 'Observatorio contra el Fraude y la Corrupción Sanitaria',
+    parent: 'Ministerio de Sanidad',
+    description:
+      '<a href="https://twitter.com/Monica_Garcia_G/status/1768227454223565199">Anunciado</a> por la ministra de Sanidad el 14 de marzo de 2024',
+
+    type: 'public',
   },
 ]
+
+const ul = document.getElementsByTagName('ul')[0]
+
+console.log(ul)
+
+ul.innerHTML = observatories
+  .map(
+    ({ name, parent, description, type }) => `
+    <li>
+      <h2>${name}</h2>
+      <p>${parent}</p>
+      <p>${description}</p>
+      <p>${types.find(({ key }) => key === type).name}</p>
+    </li>
+`
+  )
+  .join('')
 
 console.log(':)')
