026f1e0d1b54119a8bc3280cb77c172bbe7e05b4
josanri <jmsanchezrico@gmail.com>
2024-03-16 16:05:19 +0100

    Moved data into a JSON file

--- httpdocs/app.js
+++ httpdocs/app.js
@@ -1,3 +1,7 @@
+import observatories from './assets/observatories.json' assert { type: 'json' }
+
+console.log(observatories)
+
 const types = [
   {
     key: 'public',
@@ -17,34 +21,6 @@ const scopes = [
   {
     key: 'cataluna',
     name: 'Cataluña',
-  },
-]
-
-const observatories = [
-  {
-    name: 'Observatorio contra el Fraude y la Corrupción Sanitaria',
-    parent: 'Ministerio de Sanidad',
-    description:
-      '<a href="https://twitter.com/Monica_Garcia_G/status/1768227454223565199">Anunciado</a> por la ministra de Sanidad el 14 de marzo de 2024',
-    scope: 'estatal',
-    type: 'public',
-  },
-  {
-    name: 'Observatorio de Salud de las Mujeres',
-    parent:
-      'Secretaría de Estado de Sanidad | Dirección General de Salud Pública | Ministerio de Sanidad',
-    website: 'https://www.observatoriosaludmujeres.es',
-    scope: 'estatal',
-    type: 'public',
-  },
-  {
-    name: 'Observatorio del ferrocarril en España',
-    parent: 'Ministerio de Transportes y Movilidad Sostenible',
-
-    website:
-      'https://www.transportes.gob.es/ferrocarriles/observatorios/observatorio-del-ferrocarril-en-espana',
-    scope: 'estatal',
-    type: 'public',
   },
 ]
 
