1257f6339593be4084841073fc53d6ab8315d133
Carlos Garcia <solrac@solrac.ws>
2024-03-19 19:03:11 +0100

    Extender la descripción del OMHA

--- httpdocs/datasource.js
+++ httpdocs/datasource.js
@@ -157,7 +157,7 @@ const observatories = [
   {
     id: '4',
     name: "Observatori Municipal de l'Habitatge d'Alcoi",
-    description: 'El <a href="https://www.omha.alcoi.org/" alt="Website OMHA" title="Website OMHA">OMHA</a> se <a href="" alt="BOPA Constitutción OMHA" title="BOPA Contitutción OMHA">constituyó el 22-07-2020</a>, sólo se ha reunido sólo en <a href="https://www.omha.alcoi.org/es/que-es-omha/" alt="Actas de plenos" title="Actas de plenos">tres ocasiones</a> (lamentablemente, las actas de sus tres plenos no se pueden descargar.',
+    description: 'El <a href="https://www.omha.alcoi.org/" alt="Website OMHA" title="Website OMHA">OMHA</a> se <a href="https://www.omha.alcoi.org/wp-content/uploads/2022/03/TEXT-BOP-NUM-137-DE-220720-APROVACIO-DEFINITIVA.pdf" alt="BOPA Constitutción OMHA" title="BOPA Contitutción OMHA">constituyó el 22-07-2020</a>, sólo se ha reunido en <a href="https://www.omha.alcoi.org/es/que-es-omha/" alt="Actas de plenos" title="Actas de plenos">tres ocasiones</a>; lamentablemente, las actas de sus tres plenos no se pueden descargar. <br/> Tras las elecciones municipales de 2023, parece que el OMHA no ha tenido ninguna <a href="https://www.omha.alcoi.org/es/novedades/" alt="Novedades OMHA" title="Novedades OMHA">actividad</a>.',
     url: 'https://www.omha.alcoi.org/',
     scope: 'comunidad_valenciana',
     type: 'publico',
